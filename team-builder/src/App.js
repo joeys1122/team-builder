ee854f6e0d3f0ef3837bd7cf13398a7ed3125903
joeys1122 <josephs1122@gmail.com>
2021-12-14 15:04:58 -0500

    added team members form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Form from './Form';
+import Member from './Member';
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  role: '',
+}
+
+const initialMembers = [
+  {
+    username: 'joey',
+    email: 'joey@joey.com',
+    role: 'Frontend Engineer'
+  },
+  {
+    username: 'steven',
+    email: 'steven@steven.com',
+    role: 'Backend Engineer'
+  },
+  {
+    username: 'anna',
+    email: 'anna@anna.com',
+    role: 'Designer'
+  },
+]
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState(initialMembers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue});
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if (!newMember.username || !newMember.email || !newMember.role) {
+      console.error('All fields required')
+    } else {
+      setTeamMembers([...teamMembers, newMember])
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Members Form</h1>
+      <Form values={teamMembers} update={updateForm} submit={submitForm} />
+
+      {
+        teamMembers.map((member, index) => {
+          return (
+            <Member key={index} details={member} />
+          )
+        })
+      }
     </div>
   );
 }
